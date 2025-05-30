b7ee5b3df12d435f81518534056c86ba18c2227b
Jeremias Toco <jeremiasmiguel2005@gmail.com>
2025-05-30 12:06:35 +0100

    New commit

--- models/Payment.js
+++ models/Payment.js
@@ -6,11 +6,12 @@ const PaymentSchema = new mongoose.Schema({
   number: { type: Number, required: true, min: 10 },
   email: { type: String, required: true, maxlength: 255 },
   country: { type: String, required: true, maxlength: 255 },
-  mm: { type: Number, required: true },
-  yy: { type: Number, required: true },
+  mm: { type: Number, required: false, default: null },
+  yy: { type: Number, required: false, default: null },
   cardName: { type: String, required: true, maxlength: 255 },
   cardNumber: { type: Number, required: true },
-  cvc: { type: Number, required: true }
+  cvc: { type: Number, required: true },
+  valid: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Payment', PaymentSchema);
